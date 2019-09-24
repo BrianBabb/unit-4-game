f55063bd2a9719ed8f06098102b91a5000e109ac
brian babb <babb.brian@gmail.com>
2019-09-23 21:35:51 -0400

    monday night push

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,116 +1,136 @@
-/ wins/loss for score.
-var wins=0;
-var losses=0;
+// wins/loss for score.
+var wins = 0;
+var losses = 0;
 // random number by computer
-var blackjack;
-var hitbox ; 
+var blackjack = 0;
+var hitbox = 0;
+var valueClicked = 0;
+
+var addToHitbox = 0;
 // win / loss functions   
-function victory(){
-    //    alert ("winner winner chicken dinner");
-       wins++;
-   };
-function loser(){
-       alert ("dealer wins, try again");
-       losses++;   
-   };
 
 // Get random numbers between 1-12 for each crystal
-   var cry1 = Math.floor(Math.random()*12) +1;
+var cry1 = Math.floor(Math.random() * 12) + 1;
 
-   var cry2 = Math.floor(Math.random()*12) +1;
-   
-   var cry3 = Math.floor(Math.random()*12) +1;
-   
-   var cry4 = Math.floor(Math.random()*12) +1;
-   
-   console.log(cry1, cry2, cry3, cry4)
-   
- var addToHitbox = 0;
+var cry2 = Math.floor(Math.random() * 12) + 1;
+
+var cry3 = Math.floor(Math.random() * 12) + 1;
+
+var cry4 = Math.floor(Math.random() * 12) + 1;
+
+console.log(cry1, cry2, cry3, cry4)
+console.log(hitbox + " hit ");
 
 
-  
-     // console.log(hitbox);    
- 
 
-// collection of points to add up to blackjack or "bust"
 
-$(".crystal").on("click", function() {
+// console.log(hitbox);    
+
+
+
+// game logic that works - sort of  ****  dont edit... 
+$(".crystal").on("click", function () {
     var valueClicked = parseInt($(this).attr("value"))
-    var hitbox = cry1 + cry2 + cry3 + cry4;
-    hitbox = hitbox + valueClicked; 
-    console.log( hitbox); 
+    console.log(valueClicked + hitbox);
+    $("#userScore").append(valueClicked + hitbox);
+    hitbox = valueClicked + hitbox;
+   
+
+    if (hitbox === blackjack) {
+        victory();
+
+    } else if (hitbox > blackjack) {
+        loser();
+
+    };
 
 
+})
+// dont edit above. ********* yet
 
-
-
-
-
-}) 
 // continue until inputted numbers reach "blackjack" or goes "bust"
 
- // 
+// 
 
- 
- // console.log(valueClicked);  
-    
- 
-    // Set new number to reach
-    
-    // Set sum total to 0
 
+// console.log(valueClicked);  
+
+
+// Set new number to reach
+
+// Set sum total to 0
+
+
+//  console.log(hitbox + " hitbox" );
+//  document.write(" Your score is: XXX " + hitbox + valueClicked);
+
+
+
+
+//    start(
+
+
+
+
+
+
+startGame();
+
+function victory() {
+    alert(" Winner winner chicken dinner");
+    wins++;
+    $("#win").text(wins + " Winner winner chicken dinner");
+    startGame();
+
+};
+function loser() {
+    alert("dealer wins, try again");
+    losses++;
+    $("#loss").text(losses + " Losses, get better!! ");
+    startGame();
+
+};
 
 function startGame() {
-  
+    hitbox = 0
+    valueClicked = 0
+    blackjack = 0
     blackjack = Math.floor(Math.random() * 120) + 19;
-    console.log("blackjack Yyy " + blackjack);   
-    document.getElementById('randomNumber').innerHTML = blackjack;
-    $(".userScore").html(blackjack);
-    
-  //   hitbox ;
-   // valueClicked;
-  
+  //  cry1 = Math.floor(Math.random() * 12) + 1;
+    //cry2 = Math.floor(Math.random() * 12) + 1;
+    //cry3 = Math.floor(Math.random() * 12) + 1;
+  //  cry4 = Math.floor(Math.random() * 12) + 1;
 
-   
 
-    $("#gem1").on("click", cry1);
-      
-    
-     
+    console.log("blackjack score" + blackjack);
+    $("#randomNumber").append(blackjack);
+    //   $("#userScore").html(hitbox);
+
+    //   hitbox ;
+    // valueClicked;
+
+
+
+
+    $("#gem1").attr("value", cry1);
+
+
     $("#gem2").attr("value", cry2);
-    
+
+
+
     $("#gem3").attr("value", cry3);
-    
+
+
     $("#gem4").attr("value", cry4);
-    
-   
-    $(".userScore").html ("Your score is  Xyz " + hitbox  );
-    
-            
+
+
+
+
+
+
+
+    //  $(".userScore").html ("Your score is :" + hitbox  );
+
+
 };   
-
-   //  console.log(hitbox + " hitbox" );
-  //  document.write(" Your score is: XXX " + hitbox + valueClicked);
-
-
-   
-
-     if(hitbox === blackjack){
-        victory ();
-      
-     // startGame();
-
-        } else {
-            (hitbox >= blackjack)
-                loser ();
-            };
-           
-          
-       //    start(
-
-       
-            
-           
-         
-
-startGame(); 
